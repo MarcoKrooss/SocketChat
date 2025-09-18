fd2a8d528ed0cf60dd4e6cd0fd77d1c3268f6b71
MarcoKrooss <kontakt@marcokrooss.de>
2025-09-18 17:48:15 +0200

    feat: Initial commit

---
+++ server.c
@@ -0,0 +1,26 @@
+#include <sys/socket.h>
+#include <netinet/in.h>
+
+void main() {
+  int s = socket(AF_INET, SOCK_STREAM, 0);
+
+  // local ip-addr w/ port 9999
+  struct sockaddr_in addr = {
+    AF_INET,
+    0x0f27,
+    0
+  };
+
+  bind(s, &addr, sizeof(addr));
+
+  listen(s, 10);
+  int user_fd = accept(s, 0, 0);
+
+  char buf[1024] = {0};
+  int read = recv(user_fd, buf, sizeof(buf), 0);
+
+  send(read, buf, sizeof(buf), 0);
+
+  close(user_fd);
+  close(s);
+}
